2970e8389efd24b917e4064743daba580d12b54e
Jaume Parrot <jparrot@nalandaglobal.com>
2022-08-21 21:01:05 +0200

    Guard global asíncrono

--- src/router/router.js
+++ src/router/router.js
@@ -71,18 +71,43 @@ const router = createRouter({
 })
 
 // Guard Global - Síncrono
-router.beforeEach( ( to, from, next ) => {
-    // console.log({ to, from, next });
+// router.beforeEach( ( to, from, next ) => {
+//     // console.log({ to, from, next });
 
-    const random = Math.random() * 100
-    if( random > 50 ) {
-        console.log('autenticado')
-        next()
-    } else {
-        console.log(random, 'bloqueado por el beforeEach Guard')
-        next({ name: 'pokemon-home' })
-    }
-    // next()
+//     const random = Math.random() * 100
+//     if( random > 50 ) {
+//         console.log('autenticado')
+//         next()
+//     } else {
+//         console.log(random, 'bloqueado por el beforeEach Guard')
+//         next({ name: 'pokemon-home' })
+//     }
+//     // next()
+// })
+
+const canAccess = () => {
+    return new Promise( resolve => {
+
+        const random = Math.random() * 100
+        if( random > 50 ) {
+            console.log('Autenticado - canAccess')
+            resolve(true)
+        } else {
+            console.log(random, 'bloqueado por el beforeEach Guard - canAccess')
+            resolve(false)
+        }
+
+    })
+}
+
+router.beforeEach( async(to, from, next) => {
+
+    const authorized = await canAccess()
+
+    authorized 
+        ? next()
+        : next({ name: 'pokemon-home' })
+
 })
 
 export default router
